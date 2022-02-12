af1ac239b4e2a33e05435f1c38e5143a6bee1aeb
Clay Webb <clay_webb@outlook.com>
2022-02-12 17:21:39 -0600

    Client side GET created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,22 @@
 $(readyNow);
 
 function readyNow(){
-    console.log('jQuery running');
-    
+    // console.log('jQuery running');
+    $('#btn-add').on('click', newTask);
+    getList();
 } // End readyNow
+
+// <GET LAND>---------------------------------------------------------------------
+function getList(){
+    console.log('In getList');
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function (response) {
+        console.log('GET /tasks response:', response);
+        // Call the function to append the task list items
+        appendList(response);
+    }).catch(function (error) {
+        console.log('Error in the getList GET:', error);
+    });
+} // End getList function
