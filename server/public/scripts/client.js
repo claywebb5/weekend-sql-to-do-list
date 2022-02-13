05de242c61ae4d8901d14c3e039d98079b9407ab
Clay Webb <clay_webb@outlook.com>
2022-02-12 18:37:27 -0600

    The GET request now will append the table from the database to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,17 +9,47 @@ function readyNow(){
 // <GET LAND>---------------------------------------------------------------------
 function getList(){
     console.log('In getList');
+    // prevent the entire list to append each time
+    $('.list-body').empty();
     $.ajax({
         type: 'GET',
         url: '/tasks'
     }).then(function (response) {
         console.log('GET /tasks response:', response);
-        // Call the function to append the task list items
-        appendList(response);
+        // append data to the dom
+        for (let i = 0; i < response.length; i++) {
+            if (response[i].status === false) {
+                $('.list-body').append(`
+                <tr data-id=${response[i].id}>
+                    <td>${response[i].task}</td>
+                    <td>${response[i].date}</td>
+                    <td>Not Complete</td>
+                    <td><button class="completed-btn">Completed Task</button></td>
+                    <td><button class="delete-btn">Delete Task</button></td>
+                </tr>
+                `);
+            } else if (response[i].status === true) {
+                $('.list-body').append(`
+                <tr class="completed-task" data-id=${response[i].id}>
+                    <td>${response[i].task}</td>
+                    <td>${response[i].date}</td>
+                    <td>Completed!!</td>
+                    <td><button class="delete-btn">Delete Task</button></td>
+                </tr>
+                `);
+            }
+            
+        }
+        // Click listeners for the created buttons
+        $('.completed-btn').on('click', completedTask);
+        $('.delete-btn').on('click', deletedTask);
+
     }).catch(function (error) {
         console.log('Error in the getList GET:', error);
     });
 } // End getList function
 // <GET LAND>---------------------------------------------------------------------
+
+// 
 
 // <POST LAND>------------------------------------------------------------------------
